ff2550230cd06e831f9f35f7ef72d49f4e69c11b
yuki goto <yuki@yukinoMacBook-Pro.local>
2019-01-12 22:33:48 -0800

    daigaku

--- du/config/application.rb
+++ du/config/application.rb
@@ -10,7 +10,7 @@ module Du
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+ 
     config.i18n.default_locale = :ja 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -18,3 +18,5 @@ module Du
     # the framework and any gems in your application.
   end
 end
+
+config.assets.initialize_on_precompile = false
